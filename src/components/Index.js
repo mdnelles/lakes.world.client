a4ad337050600fad165537dcdcc4b6caa3501b06
mdnelles <mxnelles@gmail.com>
2020-05-07 17:12:20 -0400

    update

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,7 +4,7 @@ import { Alert } from "reactstrap";
 export const Index = () => {
    return (
       <div>
-         <Alert color='primary'>This is a primary alert — check it out!</Alert>
+         <a href='/login'>login</a>
       </div>
    );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input, Spinner } from "reactstrap";
-import { login } from "./UserFunctions";
+import { login, getCaptchaKey } from "./UserFunctions";
 import localForage from "localforage";
+import Recaptcha from "react-recaptcha";
 
 export const Login = () => {
-   const [email, setEmail] = React.useState("mxnelles@gmail.com"),
-      [password, setPassword] = React.useState("pass1WORD"),
-      [warnClass, setWarnClass] = React.useState("displayNone"),
-      [warnMsg, setWarnMsg] = React.useState(""),
-      [spinnerClass, setSpinnerClass] = React.useState("displayNone");
+   const [email, setEmail] = useState("mxnelles@gmail.com"),
+      [password, setPassword] = useState("pass1WORD"),
+      [warnClass, setWarnClass] = useState("displayNone"),
+      [captchaKey, setCaptchaKey] = useState(""),
+      [submitClass, setSubmitClass] = useState("displayNone"),
+      [warnMsg, setWarnMsg] = useState(""),
+      [spinnerClass, setSpinnerClass] = useState("displayNone");
+
+   const captcha = (event) => {
+      console.log(event);
+      setSubmitClass("displayBlock");
+   };
 
    const submit = () => {
+      setSpinnerClass("displayBlock");
+      setWarnClass("displayNone");
       if (
          email === null ||
          email === undefined ||
@@ -21,8 +31,9 @@ export const Login = () => {
       ) {
          setTimeout(() => {
             setSpinnerClass("displayNone");
+            setWarnClass("displayBlock");
          }, 500);
-         setWarnClass("displayBlock");
+
          setWarnMsg("Please enter valid credentials");
          // find number of next up slide and then update state of Cube Wrapper to trigger roll
       } else {
@@ -35,7 +46,7 @@ export const Login = () => {
                   localForage.setItem("token", res);
 
                   setTimeout(() => {
-                     //window.location.href = "/admin";
+                     window.location.href = "/admin";
                   }, 350);
                } else {
                   console.log("no token found");
@@ -49,10 +60,35 @@ export const Login = () => {
       }
    };
 
+   let captchaPlace = "";
+   if (captchaKey !== undefined && captchaKey !== "") {
+      captchaPlace = (
+         <Recaptcha
+            sitekey={captchaKey}
+            render='explicit'
+            verifyCallback={(event) => captcha(event)}
+         />
+      );
+   } else {
+      captchaPlace = "";
+   }
+
+   React.useEffect(() => {
+      console.log("Login - useEffect");
+      getCaptchaKey().then((data) => {
+         console.log(data);
+         if (data !== undefined && data.length > 10) {
+            setCaptchaKey(data);
+         } else {
+            console.log("Err: not authorized for captch key");
+         }
+      });
+   }, []);
+
    return (
       <div className='vertical-center center-outer'>
          <div className='center-inner'>
-            <div className='formBox shadow_sm'>
+            <div className='formBox shadow_blur'>
                <div style={{ textAlign: "center", width: "100%" }}>
                   <img
                      src='./img/lakesIcon.png'
@@ -63,30 +99,35 @@ export const Login = () => {
                <div className={spinnerClass}>
                   <Spinner type='grow' color='primary' /> Working...
                </div>
-               <Form>
-                  <FormGroup>
-                     <Label for='em'>Email</Label>
-                     <Input
-                        type='email'
-                        id='em'
-                        defaultValue={email}
-                        placeholder='example@me.com'
-                        onChange={(event) => setEmail(event.target.value)}
-                     />
-                  </FormGroup>
-                  <FormGroup>
-                     <Label for='pw'>Password</Label>
-                     <Input
-                        type='password'
-                        id='pw'
-                        defaultValue={password}
-                        placeholder=''
-                        onChange={(event) => setPassword(event.target.value)}
-                     />
-                  </FormGroup>
+               <br />
+               {captchaPlace}
+               <br />
+               <div className={submitClass}>
+                  <Form>
+                     <FormGroup>
+                        <Label for='em'>Email</Label>
+                        <Input
+                           type='email'
+                           id='em'
+                           defaultValue={email}
+                           placeholder='example@me.com'
+                           onChange={(event) => setEmail(event.target.value)}
+                        />
+                     </FormGroup>
+                     <FormGroup>
+                        <Label for='pw'>Password</Label>
+                        <Input
+                           type='password'
+                           id='pw'
+                           defaultValue={password}
+                           placeholder=''
+                           onChange={(event) => setPassword(event.target.value)}
+                        />
+                     </FormGroup>
 
-                  <Button onClick={submit}>Submit</Button>
-               </Form>
+                     <Button onClick={submit}>Submit</Button>
+                  </Form>
+               </div>
                <br />
                <div className={"alert-warning " + warnClass}>{warnMsg}</div>
             </div>
